8838e1c6bd752a315244b0768eb63c310e5869e9
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2026-01-31 16:41:32 +0530

    add Async Write, Read File System + Intro

---
+++ File_System/app.js
@@ -0,0 +1,13 @@
+// const fs = require("fs");
+
+import fs from "fs";
+
+fs.readFile("example.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+  console.log("File contents:", data);
+});
+
+console.log("This runs first");
