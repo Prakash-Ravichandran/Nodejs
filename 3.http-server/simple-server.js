200f6820b6f93211aeeb56a5074436d676c34ab1
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2026-01-31 21:18:30 +0530

    server routing

--- 3.http-server/simple-server.js
+++ 3.http-server/simple-server.js
@@ -1,6 +1,17 @@
 import http from "http";
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("Home Page!");
+  } else if (req.url == "/about") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("About Page!");
+  } else {
+    res.writeHead(404, { "content-type": "text/plain" });
+    res.end("404 Not Found");
+  }
+});
 
 server.listen(5000, () => {
   console.log("Server is listening on port 5000");
