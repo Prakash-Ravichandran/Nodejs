99f0375e0237f57bdf5a90975e6e9e61d32393d0
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2026-02-15 12:34:01 +0530

    add middlewares

--- 4.expressjs/expressapp/index.js
+++ 4.expressjs/expressapp/index.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+// Middleware
+app.use((req, res, next) => {
+  console.log("Request received");
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // constructing this in a simple server requires some conditions like req.method == "GET"
 app.get("/", (req, res) => {
   res.status(200).send("Hello Express App");
@@ -11,6 +18,17 @@ app.post("/", (req, res) => {
   res.status(200).send("POST request received");
 });
 
+// Default match for all other routes
+app.use((req, res) => {
+  res.status(404).send("Route not found");
+});
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
+
+/** Middleware (actions between request and response cycle) usages
+ * 1. For hanndling default matches
+ * 2. Before request handler gets starts
+ * 3. Logging
+ */
