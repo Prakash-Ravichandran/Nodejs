05527313443fa269a3ad00d334e743be84afb69c
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2026-03-02 11:42:43 +0530

    delete Node js files

--- 2.core_modules/synchronousFs.js
+++
@@ -1,6 +0,0 @@
-import fs from "fs";
-
-fs.writeFileSync("./sync-example.txt", "Node JS - Sync File Writing Demo");
-
-const data = fs.readFileSync("./sync-example.txt", "utf-8");
-console.log("Reading File contents synchronously:", data);
